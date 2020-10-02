f576e16ccb157f358a8a6940ac368ceda8415e45
Abhijeet Dhamankar <adhamankar@gmail.com>
2020-10-02 21:32:52 +0530

    Update templates

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 var static = require('node-static');
 var http = require('http');
+require('dotenv').config();
 
-var file = new (static.Server)();
+console.log(`Configuring server at PORT ${process.env.PORT}`);
+
+var file = new static.Server();
 
 http.createServer(function (req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    file.serve(req, res);
-}).listen(8080);
+    req.addListener('end', () => {
+        file.serve(req, res, (e, result) => {
+            console.info(`Serving ${req.url}`);
+        });
+    }).resume();
+}).listen(process.env.PORT);
